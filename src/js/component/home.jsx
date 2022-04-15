5be7898bd56ce9bef249feb4ba5aaf35c88b37d7
jedens5000 <mrjonedens@gmail.com>
2022-04-15 20:55:02 +0000

    Need to add the Add Task functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./Header";
+import Tasks from "./Tasks";
 
 const Home = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Doctors Appt",
+			day: "Feb 5th at 2:30pm",
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: "Meeting at School",
+			day: "Feb 6th at 1:30pm",
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: "Grocery Shopping",
+			day: "Feb 5th at 2:30pm",
+			reminder: false,
+		},
+	]);
+
+	//Delete Task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
+
 	return (
 		<div className="container">
 			{/* <h1>Test Home.jsx</h1> */}
 			<Header />
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} />
+			) : (
+				"No Tasks Available"
+			)}
 		</div>
 	);
 };
